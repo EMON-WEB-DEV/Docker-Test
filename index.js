6e45172b003f1a94b33ff142acffe81d6f85d7b2
EMON <jakariachowdhury2427@gmail.com>
2026-04-19 21:42:58 +0600

    feat: enhance greeting message to include hostname and platform information

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const os = require('os');
+
+const hostname = os.hostname();
+const platform = os.platform();
+
 
 app.get('/', (req, res) => {
-  res.send('Hello Docker!');
+  res.send(`Hello Docker! ${hostname} is running on ${platform}.`);
 });
 
 app.listen(port, () => {
